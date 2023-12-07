10170a41aa08b6e2e2b7838829e11818eba07d8f
jmbeba <john.mbeba@student.moringaschool.com>
2023-12-07 22:01:10 +0300

    lab completed

---
+++ migrations/versions/446991bf179a_second_init.py
@@ -0,0 +1,24 @@
+"""Second Init
+
+Revision ID: 446991bf179a
+Revises: 7a8a965f48ba
+Create Date: 2023-12-07 21:49:51.073919
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '446991bf179a'
+down_revision = '7a8a965f48ba'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ migrations/versions/7a8a965f48ba_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 7a8a965f48ba
+Revises: 
+Create Date: 2023-12-07 21:48:08.033859
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7a8a965f48ba'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
